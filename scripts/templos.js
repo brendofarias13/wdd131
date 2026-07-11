4de76c83d9d17b5addd6652a05211408956e58ef
Seu Nome <seu_email@example.com>
2026-07-11 20:55:47 -0300

    templos

---
+++ scripts/templos.js
@@ -0,0 +1,18 @@
+const menu=document.querySelector("#menu");
+const nav=document.querySelector(".navigation");
+
+menu.addEventListener("click",()=>{
+
+nav.classList.toggle("open");
+
+if(nav.classList.contains("open")){
+menu.textContent="✖";
+}else{
+menu.textContent="☰";
+}
+
+});
+
+document.querySelector("#year").textContent=new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent="Última Modificação: "+document.lastModified;
