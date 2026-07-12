c6fca86ae46fb57c0382a83aeff980bd85935499
Seu Nome <seu_email@example.com>
2026-07-11 21:47:50 -0300

    agota vai

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,19 +1,28 @@
 const menu = document.querySelector("#menu");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".navigation");
 
 menu.addEventListener("click", () => {
-
     nav.classList.toggle("open");
-
-    if(nav.classList.contains("open")){
-        menu.textContent = "✖";
-    }else{
-        menu.textContent = "☰";
-    }
-
+    menu.textContent = nav.classList.contains("open") ? "✖" : "☰";
 });
 
+// Rodapé
 document.querySelector("#year").textContent = new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent =
 "Última Modificação: " + document.lastModified;
+
+// Navegação
+const links = document.querySelectorAll(".navigation a");
+
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // Remove a classe ativa
+        links.forEach(l => l.classList.remove("active"));
+
+        // Adiciona ao link clicado
+        link.classList.add("active");
+    });
+});
